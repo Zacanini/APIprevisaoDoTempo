2ef5e090d66cf91a1d1f9f9cfb162c7acc06ef51
Matheus De O Zacanini <zacanini2@gmail.com>
2024-05-29 11:28:23 -0300

    fix: <Adicionando correção de erro para caso o usuario nãodigite nada na pesquisa>

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function colocarDadosNaTela(dados) {
         document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
     } catch (error) {
         document.querySelector(".cidade").innerHTML = "Cidade não encontrada! verifique o nome escrito!"
-        document.querySelector(".pais").innerHTML = "PAÍS: --" 
+        document.querySelector(".pais").innerHTML = "PAÍS: --"
         document.querySelector(".temp").innerHTML = "°c"
-        document.querySelector(".lat").innerHTML = "LATITUDE: " 
-        document.querySelector(".lon").innerHTML = "LONGITUDE: " 
+        document.querySelector(".lat").innerHTML = "LATITUDE: "
+        document.querySelector(".lon").innerHTML = "LONGITUDE: "
         document.querySelector(".wind").innerHTML = "VENTO: m/s"
         document.querySelector(".tezto-previsao").innerHTML = ""
         document.querySelector(".umidade").innerHTML = "UMIDADE: %"
@@ -29,20 +29,33 @@ function colocarDadosNaTela(dados) {
 
 
 async function BuscarCidade(cidade) {
-     // Exibir o loading
-  document.getElementById("load").classList.add("loading");
 
-  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${keyApi}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    // Exibir o loading
+    document.getElementById("load").classList.add("loading");
 
-  // Remover o loading após 1.5 segundos
-  setTimeout(() => {
-    document.getElementById("load").classList.remove("loading");
-  }, 215);
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${keyApi}&lang=pt_br&units=metric`).then(resposta => resposta.json())
 
-  setTimeout(()=>{
-    colocarDadosNaTela(dados);
-  },216);
-  
+    // Remover o loading após 1.5 segundos
+    setTimeout(() => {
+        document.getElementById("load").classList.remove("loading");
+    }, 215);
+    if (cidade === "") {
+        document.querySelector(".cidade").innerHTML = "Digite o nome de alguma cidade "
+        document.querySelector(".pais").innerHTML = "PAÍS: --"
+        document.querySelector(".temp").innerHTML = "°c"
+        document.querySelector(".lat").innerHTML = "LATITUDE: "
+        document.querySelector(".lon").innerHTML = "LONGITUDE: "
+        document.querySelector(".wind").innerHTML = "VENTO: m/s"
+        document.querySelector(".tezto-previsao").innerHTML = ""
+        document.querySelector(".umidade").innerHTML = "UMIDADE: %"
+        document.querySelector(".img-previsao").src = ``
+        return
+    }
+
+    setTimeout(() => {
+        colocarDadosNaTela(dados);
+    }, 216);
+
 }
 
 document.querySelector(".imput-city").addEventListener("keyup", function (event) {
