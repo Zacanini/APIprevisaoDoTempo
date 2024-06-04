b093cb27a17d731880bd17b316c6b5b7104ee820
Matheus De O Zacanini <zacanini2@gmail.com>
2024-06-04 15:39:36 -0300

    Colocando api de noticias para imprimir no console

--- APIprevisaoDoTempo-main/script.js
+++ APIprevisaoDoTempo-main/script.js
@@ -1,8 +1,23 @@
-const keyApi = "e953f74cd6e4f5324c0b52713a2a3abb"
-const botaoModo = document.querySelectorAll('.botao')
+const keyApi = "e953f74cd6e4f5324c0b52713a2a3abb";
+const cidade = document.querySelector(".imput-city");
+const botaoModo = document.querySelectorAll('.botao');
+const keyApiNews = "5e36fe1ac5f940e2856e7906b6602298";
+
 
 
 function colocarDadosNaTela(dados) {
+    if (cidade.value === "") {
+        document.querySelector(".cidade").innerHTML = "Digite o nome de alguma cidade "
+        document.querySelector(".pais").innerHTML = "PAÍS: --"
+        document.querySelector(".temp").innerHTML = "°c"
+        document.querySelector(".lat").innerHTML = "LATITUDE: "
+        document.querySelector(".lon").innerHTML = "LONGITUDE: "
+        document.querySelector(".wind").innerHTML = "VENTO: m/s"
+        document.querySelector(".tezto-previsao").innerHTML = ""
+        document.querySelector(".umidade").innerHTML = "UMIDADE: %"
+        document.querySelector(".img-previsao").src = ``
+        return
+    }
     try {
         console.log(dados)
         document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
@@ -34,27 +49,21 @@ async function BuscarCidade(cidade) {
     // Exibir o loading
     document.getElementById("load").classList.add("loading");
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${keyApi}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${keyApi}&lang=pt_br&units=metric`);
+    const data1 = await dados.json();
+    var url = `https://newsapi.org/v2/everything?q=`;
+    const response = await fetch(`${url} cidade de ${cidade}&apiKey=${keyApiNews}`);
+    const data2 = await response.json();
+    console.log(data2);
 
-    // Remover o loading após 1.5 segundos
+
+    // Remover o loading após -- segundos
     setTimeout(() => {
         document.getElementById("load").classList.remove("loading");
     }, 215);
-    if (cidade === "") {
-        document.querySelector(".cidade").innerHTML = "Digite o nome de alguma cidade "
-        document.querySelector(".pais").innerHTML = "PAÍS: --"
-        document.querySelector(".temp").innerHTML = "°c"
-        document.querySelector(".lat").innerHTML = "LATITUDE: "
-        document.querySelector(".lon").innerHTML = "LONGITUDE: "
-        document.querySelector(".wind").innerHTML = "VENTO: m/s"
-        document.querySelector(".tezto-previsao").innerHTML = ""
-        document.querySelector(".umidade").innerHTML = "UMIDADE: %"
-        document.querySelector(".img-previsao").src = ``
-        return
-    }
 
     setTimeout(() => {
-        colocarDadosNaTela(dados);
+        colocarDadosNaTela(data1);
     }, 216);
 
 }
@@ -66,12 +75,18 @@ document.querySelector(".imput-city").addEventListener("keyup", function (event)
     }
 });
 
-
-
 function CliqueiNoBotao() {
-    const cidade = document.querySelector(".imput-city").value
-    BuscarCidade(cidade)
+    BuscarCidade(cidade.value)
 }
+
+
+
+
+
+
+
+
+
 
 
 let imagemAtual = 'imgs/ceu.png';
@@ -89,25 +104,29 @@ botaoModo.forEach(botao => {
             iconeAtual = 'imgs/procurar (1).png';
         }
 
-        document.querySelector('.class-img').src = iconeAtual;
-        document.querySelector('.icon').src = imagemAtual;
-        document.querySelector('.imput-city').classList.toggle('iput-noturno');
-        document.querySelector('.class-busca').classList.toggle('class-busca-noturno');
-        document.querySelector('body').classList.toggle('noturnoFundo');
-        document.querySelector('.caixa-maior').classList.toggle('noturnoContainer');
-        document.querySelector('.caixa-tempo').classList.toggle('noturnoTempo');
-        document.querySelector('.cidade').classList.toggle('letrasNoturno');
-        document.querySelector('.pais').classList.toggle('letrasNoturno');
-        document.querySelector('.temp').classList.toggle('letrasNoturno');
-        document.querySelector('.tezto-previsao').classList.toggle('letrasNoturno');
-        document.querySelector('.umidade').classList.toggle('letrasNoturno');
-        document.querySelector('.wind').classList.toggle('letrasNoturno');
-        document.querySelector('.lat').classList.toggle('letrasNoturno');
-        document.querySelector('.lon').classList.toggle('letrasNoturno');
-        document.querySelector('.botao').classList.toggle('botao-noturno');
+        modoNoturno();
 
     });
 });
+
+function modoNoturno() {
+    document.querySelector('.class-img').src = iconeAtual;
+    document.querySelector('.icon').src = imagemAtual;
+    document.querySelector('.imput-city').classList.toggle('iput-noturno');
+    document.querySelector('.class-busca').classList.toggle('class-busca-noturno');
+    document.querySelector('body').classList.toggle('noturnoFundo');
+    document.querySelector('.caixa-maior').classList.toggle('noturnoContainer');
+    document.querySelector('.caixa-tempo').classList.toggle('noturnoTempo');
+    document.querySelector('.cidade').classList.toggle('letrasNoturno');
+    document.querySelector('.pais').classList.toggle('letrasNoturno');
+    document.querySelector('.temp').classList.toggle('letrasNoturno');
+    document.querySelector('.tezto-previsao').classList.toggle('letrasNoturno');
+    document.querySelector('.umidade').classList.toggle('letrasNoturno');
+    document.querySelector('.wind').classList.toggle('letrasNoturno');
+    document.querySelector('.lat').classList.toggle('letrasNoturno');
+    document.querySelector('.lon').classList.toggle('letrasNoturno');
+    document.querySelector('.botao').classList.toggle('botao-noturno');
+}
 
 
 
